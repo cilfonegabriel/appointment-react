39b27d1236d544bada4073066aea7fabe455982c
gabicilfone <cilfonegabriel@gmail.com>
2023-09-11 18:30:05 -0300

    show text conditionally

--- src/components/PatientList.jsx
+++ src/components/PatientList.jsx
@@ -2,21 +2,35 @@ import Patient from "./Patient"
 
 const PatientList = ({patients}) => {
 
+
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
 
-      <h2 className="font-black text-3xl text-center">Patient list</h2>
-      <p className="text-xl mt-5 mb-10 text-center">
-        Manage your {''}
-        <span className="text-indigo-600 font-bold">Patients and lists</span>
-      </p>
+      {patients && patients.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">Patient list</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Manage your {''}
+            <span className="text-indigo-600 font-bold">Patients and lists</span>
+          </p>
 
-      { patients.map( patient => (
-        <Patient
-            key={patient.id}
-            patient={patient}
-        />
-      ))}
+          { patients.map( patient => (
+            <Patient
+                key={patient.id}
+                patient={patient}
+            />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">There are no patients</h2>
+            <p className="text-xl mt-5 mb-10 text-center">
+              Start adding patients {''}
+              <span className="text-indigo-600 font-bold">and will be added below</span>
+            </p>
+        </>
+      )}
+
     </div>
   )
 }
