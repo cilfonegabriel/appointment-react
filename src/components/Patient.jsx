030594135b7d6dcd1ba5fd970df68b247a0a6e09
gabicilfone <cilfonegabriel@gmail.com>
2023-09-11 09:07:14 -0300

    add patient list, style and component

---
+++ src/components/Patient.jsx
@@ -0,0 +1,24 @@
+const Patient = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+    <p className="font-bold mb-3 text-gray-700 uppercase">Name: {''}
+        <span className="font-normal normal-case">Hook</span>
+    </p>
+    <p className="font-bold mb-3 text-gray-700 uppercase">Owner: {''}
+        <span className="font-normal normal-case">Gabriel</span>
+    </p>
+    <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
+        <span className="font-normal normal-case">correo@correo.com</span>
+    </p>
+    <p className="font-bold mb-3 text-gray-700 uppercase">High date: {''}
+        <span className="font-normal normal-case">September 10, 2023</span>
+    </p>
+    <p className="font-bold mb-3 text-gray-700 uppercase">Symptoms: {''}
+        <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae ab, impedit nostrum fuga soluta recusandae repellendus. Sint perspiciatis numquam necessitatibus sit asperiores perferendis quidem, itaque eum natus at magnam repellat.</span>
+    </p>
+    
+  </div>
+  )
+}
+
+export default Patient
